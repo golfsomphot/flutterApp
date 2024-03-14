1121a0112d4a4207437098cb08300d54cb6a0273
golf <142736400+golfsomphot@users.noreply.github.com>
2024-03-14 20:00:56 +0700

    width
    width

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -51,7 +51,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             const SizedBox(
-              width: 5,
+              width: 10,
             ),
             Center(
               child: SizedBox(
