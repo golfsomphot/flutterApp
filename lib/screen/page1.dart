37f1f904db229b1e0957569c65e4f3c868bf8896
golf <142736400+golfsomphot@users.noreply.github.com>
2024-03-15 02:05:46 +0700

    Update page1.dart

--- lib/screen/page1.dart
+++ lib/screen/page1.dart
@@ -14,7 +14,7 @@ class _Page1State extends State<Page1> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'myapptest',
+      title: '1',
       routes: AppRoute.all,
       home: Login(),
     );
