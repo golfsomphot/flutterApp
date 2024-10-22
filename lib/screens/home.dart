3009bf39b4267a06fddab32155c65b1dd08e2bf4
golf <142736400+golfsomphot@users.noreply.github.com>
2024-10-22 23:28:44 +0700

    ว

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,64 +11,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-<<<<<<< HEAD
-  String _scanResult = 'Unknown';
-
-  Future<void> startBarcodeScan() async {
-    String barcodeScanRes;
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-        '#ff6666', // สีของปุ่ม "Cancel"
-        'Cancel', // ข้อความของปุ่ม "Cancel"
-        true, // เปิดใช้งานแฟลชไฟฉายหรือไม่
-        ScanMode.BARCODE, // หรือใช้ ScanMode.QR สำหรับ QR Code
-      );
-    } catch (e) {
-      barcodeScanRes = 'Failed to get scan result: $e';
-    }
-
-    if (!mounted) return;
-
-    setState(() {
-      _scanResult = barcodeScanRes;
-    });
-
-    print('Scan result: $_scanResult');
-  }
-
-=======
->>>>>>> parent of f125523 (scan)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
-        automaticallyImplyLeading: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              'HOME',
-              style: TextStyle(fontWeight: FontWeight.w400),
-            ),
-            Row(
-              children: [
-                IconButton(
-                  focusColor: Colors.amber,
-                  onPressed: () => {},
-                  icon: FaIcon(FontAwesomeIcons.add),
-                ),
-                IconButton(
-                  focusColor: Colors.amber,
-                  onPressed: startBarcodeScan,
-                  icon: FaIcon(FontAwesomeIcons.addressBook),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-=======
           automaticallyImplyLeading: false,
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -93,7 +39,6 @@ class _HomeState extends State<Home> {
               )
             ],
           )),
->>>>>>> parent of f125523 (scan)
       body: ListMenu(),
     );
   }
