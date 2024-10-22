a9c31debab3a6da39753d73b8da2ecabf0493ecc
golf <142736400+golfsomphot@users.noreply.github.com>
2024-10-22 22:27:44 +0700

    cc

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:myapp/wigget/listmenu.dart';
@@ -10,34 +11,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String _scanResult = 'Unknown';
+
+  Future<void> startBarcodeScan() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666', // สีของปุ่ม "Cancel"
+        'Cancel', // ข้อความของปุ่ม "Cancel"
+        true, // เปิดใช้งานแฟลชไฟฉายหรือไม่
+        ScanMode.BARCODE, // หรือใช้ ScanMode.QR สำหรับ QR Code
+      );
+    } catch (e) {
+      barcodeScanRes = 'Failed to get scan result: $e';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _scanResult = barcodeScanRes;
+    });
+
+    print('Scan result: $_scanResult');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'HOME',
-                style: TextStyle(fontWeight: FontWeight.w400),
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    focusColor: Colors.amber,
-                    onPressed: () {},
-                    icon: FaIcon(FontAwesomeIcons.add),
-                  ),
-                  IconButton(
-                    focusColor: Colors.amber,
-                    onPressed: () {},
-                    icon: FaIcon(FontAwesomeIcons.camera),
-                  )
-                ],
-              )
-            ],
-          )),
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'HOME',
+              style: TextStyle(fontWeight: FontWeight.w400),
+            ),
+            Row(
+              children: [
+                IconButton(
+                  focusColor: Colors.amber,
+                  onPressed: () => {},
+                  icon: FaIcon(FontAwesomeIcons.add),
+                ),
+                IconButton(
+                  focusColor: Colors.amber,
+                  onPressed: startBarcodeScan,
+                  icon: FaIcon(FontAwesomeIcons.addressBook),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
       body: ListMenu(),
     );
   }
